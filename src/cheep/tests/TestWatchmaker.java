f4cc392dd708a334ebfa968de76725f45537fadf
Mateus Araujo Borges <MateusAraujoBorges@gmail.com>
2011-09-27 12:34:37 -0300

    try to maximize features after the end of evolutionary process

--- src/cheep/tests/TestWatchmaker.java
+++ src/cheep/tests/TestWatchmaker.java
@@ -3,6 +3,9 @@ package cheep.tests;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
+
+import javax.management.RuntimeErrorException;
 
 import operator.ProductSetMutation;
 import operator.ValidProductSetMutation;
@@ -63,82 +66,139 @@ public class TestWatchmaker {
 						new Probability(0.75)), 1, true, rng);
 		
 		engine.addEvolutionObserver(new EvolutionLogger());
-		List<EvaluatedCandidate<ProductSet>> finalPopulation = engine.evolvePopulation(100, // 100 individuals in the population.
+		List<EvaluatedCandidate<ProductSet>> finalPopulation = engine.evolvePopulation(50, // 100 individuals in the population.
 				5, // 5% elitism.
 //				new GenerationCount(500), new Stagnation(20, true));
 				new GenerationCount(500));
 	
-		for (EvaluatedCandidate<ProductSet> ec : finalPopulation) {
-			System.out.printf("Final Element: maxFitness:%f size:%d \n",ec.getFitness(),ec.getCandidate().getProducts().size());
-			for(Product p : ec.getCandidate().getProducts()) {
-				System.out.println(GPLProductValidator.printProduct(p.getFeatures()));
-			}
-			for(Product p : ec.getCandidate().getProducts()) {
-				System.out.println(GPLProductValidator.print01(p.getFeatures()));
-			}
-		}
-	}
-
-	
-	public static void main(String[] args) {
-//		driver();
-		runGPLWithSteadyEngine();
-	}
-
-
-	private static void driver() {
-		ProductValidator valP = new GPLProductValidator();
-		Validator<ProductSet> valPset = new ProductSetValidator(valP, 0.9, 0.99);
-
-//		CandidateFactory<Product> pfac = new ProductFactory(15);
-//		CandidateFactory<ProductSet> psfac = new ProductSetFactory(pfac, 10);
 		
-		CandidateFactory<Product> validProdFac = new ValidProductFactory(15,valP);
-//		CandidateFactory<Product> pfac = new ValidProductFactory(42,valP);
+		EvaluatedCandidate<ProductSet> best = finalPopulation.get(0);
+		System.out.printf("Final Element: maxFitness:%f size:%d \n",best.getFitness(),best.getCandidate().getProducts().size());		
+		for(Product p : best.getCandidate().getProducts()) {
+			System.out.println(GPLProductValidator.printProduct(p.getFeatures()));
+		}
+		for(Product p : best.getCandidate().getProducts()) {
+			System.out.println(GPLProductValidator.print01(p.getFeatures()));
+		}
+		
+		ProductSet maxSet = maximizeFeatures(best.getCandidate(), valP, valPset);
+		
+		System.out.println();
+		
+		System.out.printf("Maximized Final Element: maxFitness:%f size:%d \n",valPset.getFitness(maxSet, null),maxSet.getProducts().size());		
+		for(Product p : maxSet.getProducts()) {
+			System.out.println(GPLProductValidator.printProduct(p.getFeatures()));
+		}
+		for(Product p : maxSet.getProducts()) {
+			System.out.println(GPLProductValidator.print01(p.getFeatures()));
+		}
+		
+//		for (EvaluatedCandidate<ProductSet> ec : finalPopulation) {
+//			System.out.printf("Final Element: maxFitness:%f size:%d \n",ec.getFitness(),ec.getCandidate().getProducts().size());
+//			for(Product p : ec.getCandidate().getProducts()) {
+//				System.out.println(GPLProductValidator.printProduct(p.getFeatures()));
+//			}
+//			for(Product p : ec.getCandidate().getProducts()) {
+//				System.out.println(GPLProductValidator.print01(p.getFeatures()));
+//			}
+//		}
+	}
+
+	public static void runBerkleyWithSteadyEngine() {
+		ProductValidator valP = new BerkleyProductValidator();
+		Validator<ProductSet> valPset = new ProductSetValidator(valP, 0.9, 0.99);
+		
+		//used only for seeding the population 
+		CandidateFactory<Product> validProdFac = new ValidProductFactory(42,valP);
 		CandidateFactory<ProductSet> validProdSetFac = new ValidProductSetFactory(validProdFac, 10,valPset);
 		
-//		List<EvolutionaryOperator<ProductSet>> a = new ArrayList<EvolutionaryOperator<ProductSet>>(3);
-//		a.add(new ProductSetMutation(new Probability(0.50), new Probability(0.25), new Probability(0.20), pfac));
-
 		List<EvolutionaryOperator<ProductSet>> a = new ArrayList<EvolutionaryOperator<ProductSet>>(3);
-		a.add(new ValidProductSetMutation(new Probability(1), new Probability(0.25), new Probability(0.20), validProdFac,valPset,false));
+
+		a.add(new ValidProductSetMutation(new Probability(1), new Probability(0.20), new Probability(0.15), validProdFac,valPset,true));
 		
 		EvolutionaryOperator<ProductSet> pipeline = new EvolutionPipeline<ProductSet>(a);
 		Random rng = new MersenneTwisterRNG();
 		EvolutionEngine<ProductSet> engine = new SteadyStateEvolutionEngine<ProductSet>(
 				validProdSetFac, pipeline, valPset, new TournamentSelection(
 						new Probability(0.75)), 1, true, rng);
-//		EvolutionEngine<ProductSet> engine = new GenerationalEvolutionEngine<ProductSet>(
-//				validProdSetFac, pipeline, valPset, new SigmaScaling(), rng);
 		
 		engine.addEvolutionObserver(new EvolutionLogger());
-		List<EvaluatedCandidate<ProductSet>> finalPopulation = engine.evolvePopulation(50, // 100 individuals in the population.
-				5, // 5% elitism.
+		List<EvaluatedCandidate<ProductSet>> finalPopulation = engine.evolvePopulation(50, // 50 individuals in the population.
+				5, // 10% elitism.
 //				new GenerationCount(500), new Stagnation(20, true));
 				new GenerationCount(500));
 	
-//		for (EvaluatedCandidate<ProductSet> ec : finalPopulation) {
-//			System.out.printf("Final Element: maxFitness:%f size:%d %s\n",ec.getFitness(),ec.getCandidate().getProducts().size(),ec.getCandidate());
-//			
-//		}
-		System.out.println("Best product:");
 		EvaluatedCandidate<ProductSet> best = finalPopulation.get(0);
-		System.out.printf("Final Element: maxFitness:%f size:%d %s\n",best.getFitness(),best.getCandidate().getProducts().size(),best.getCandidate());
+		System.out.printf("Final Element: maxFitness:%f size:%d \n",best.getFitness(),best.getCandidate().getProducts().size());		
+		for(Product p : best.getCandidate().getProducts()) {
+			System.out.println(BerkleyProductValidator.printProduct(p.getFeatures()));
+		}
+		for(Product p : best.getCandidate().getProducts()) {
+			System.out.println(BerkleyProductValidator.print01(p.getFeatures()));
+		}
 		
-		int i = 1;
-		for (EvaluatedCandidate<ProductSet> finalElement : finalPopulation) {
-			if(valPset.validate(finalElement.getCandidate())){
-				System.out.println("Final element no. " +i+ " is valid");
-				for(Product p : best.getCandidate().getProducts()) {
-					System.out.println(GPLProductValidator.printProduct(p.getFeatures()));
-//					System.out.println(BerkleyProductValidator.printProduct(p.getFeatures()));
-//				    System.out.println(BerkleyProductValidator.print01(p.getFeatures()));
-					System.out.println(GPLProductValidator.print01(p.getFeatures()));
-				
+		ProductSet maxSet = maximizeFeatures(best.getCandidate(), valP, valPset);
+		
+		System.out.println();
+		
+		System.out.printf("Maximized Final Element: maxFitness:%f size:%d \n",valPset.getFitness(maxSet, null),maxSet.getProducts().size());		
+		for(Product p : maxSet.getProducts()) {
+			System.out.println(BerkleyProductValidator.printProduct(p.getFeatures()));
+		}
+		for(Product p : maxSet.getProducts()) {
+			System.out.println(BerkleyProductValidator.print01(p.getFeatures()));
+		}
+		
+//		for (EvaluatedCandidate<ProductSet> ec : finalPopulation) {
+//			System.out.printf("Final Element: maxFitness:%f size:%d \n",ec.getFitness(),ec.getCandidate().getProducts().size());
+//			for(Product p : ec.getCandidate().getProducts()) {
+//				System.out.println(BerkleyProductValidator.printProduct(p.getFeatures()));
+//			}
+//			for(Product p : ec.getCandidate().getProducts()) {
+//				System.out.println(BerkleyProductValidator.print01(p.getFeatures()));
+//			}
+//		}
+	}
+
+	//flip all the features turned off that don't invalidate the product
+	public static ProductSet maximizeFeatures(ProductSet toMaximize, Validator<Product> valp, Validator<ProductSet> valpset) {
+		Set<Product> pset = toMaximize.getProducts();
+		Product[] products = new Product[pset.size()];
+		
+		int i = 0;
+		for(Product p: pset) {
+			boolean[] features = p.getFeatures();
+			boolean[] fclone = features.clone();
+			
+			for(int j = 0; j < features.length; j++) {
+				if(fclone[j] == false) {
+					fclone[j] = true;
+					Product tmpProd = new Product(fclone);
+					
+					if(valp.validate(tmpProd)) { //found an optional feature turned off
+						continue;
+					} else {
+						fclone[j] = false;
+					}
 				}
 			}
+			
+			Product maximizedProd = new Product(fclone);
+			products[i] = maximizedProd;
 			i++;
 		}
+		
+		ProductSet maxPset = new ProductSet(products);
+		if (valpset.validate(maxPset)) {
+			return maxPset;
+		} else { //should not happen - all the products are valid!
+			throw new RuntimeException("What happened here?");
+		}
+	}
+	
+	public static void main(String[] args) {
+//		runGPLWithSteadyEngine();
+		runBerkleyWithSteadyEngine();
 	}
 }
 
